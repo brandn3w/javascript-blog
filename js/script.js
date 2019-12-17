094a2ecb728549f32bfc024386e77c3a8fce228d
Nadja Brandys <nadiusza@gmail.com>
2019-12-17 22:49:23 +0100

    Add active class to clicked articles

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
+'use strict';
+
 /*document.getElementById('test-button').addEventListener('click', function(){
   const links = document.querySelectorAll('.titles a');
   console.log('links:', links);
 });*/
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!', event);
   
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -13,9 +16,12 @@ const titleClickHandler = function(event){
       activeLink.classList.remove('active');
     }
   
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+
+    console.log('clickedElement:', clickedElement);  
+
   
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.posts .active');
 
@@ -27,10 +33,11 @@ const titleClickHandler = function(event){
   
     /* find the correct article using the selector (value of 'href' attribute) */
   
-    /* add class 'active' to the correct article */
+    /* add class 'active' to the correct article !!! */
+    
+    clickedElement.classList.add('active');
 
 
-  }
   
   const links = document.querySelectorAll('.titles a');
   

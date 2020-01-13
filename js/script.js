98d777b8ea181c28d01cc96e35192876786dcd56
Nadja Brandys <nadiusza@gmail.com>
2020-01-13 19:55:16 +0100

    Add handlebars for article link

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 "use strict";
-
+const templates = {
+  articleLink: Handlebars.compile(document.querySelector('#template-article-link').innerHTML)
+}
 
 const optArticleSelector = ".post",
   optTitleSelector = ".post-title",
@@ -89,8 +91,8 @@ function generateTitleLinks(customSelector = '') {     //przypisanie domyślnej 
     /* get the title from the title element */
 
     /* create HTML of the link */
-    const linkHTML = '<li><a href="#' + articleId + '"><span>' +
-      articleTitle + "</span></a></li>";
+    const linkHTMLData = {id: articleId, title: articleTitle};
+    const linkHTML = templates.articleLink(linkHTMLData);
     console.log(linkHTML);
 
     /* insert link into html variable */
